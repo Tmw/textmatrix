1309dd953e2fcbc4b8a7fd969ad05e8965c29a55
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2020-02-17 22:49:01 +0100

    Prepare mixfile

--- mix.exs
+++ mix.exs
@@ -1,13 +1,18 @@
 defmodule Textmatrix.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :textmatrix,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      docs: docs()
     ]
   end
 
@@ -23,6 +28,33 @@ defmodule Textmatrix.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    """
+    Textmatrix in Elixir. Make working with 2D text a breeze.
+    """
+  end
+
+  defp package() do
+    [
+      maintainers: ["Tiemen Waterreus"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/tmw/textmatrix"}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      name: "Texstmatrix",
+      source_ref: "v#{@version}",
+      canonical: "http://hexdocs.pm/textmatrix",
+      source_url: "https://github.com/tmw/textmatrix",
+      extras: [
+        "README.md"
+      ]
     ]
   end
 end
